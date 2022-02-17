dc02f9d90d33dc1ad8f4a41da7debee60b498a09
LeonShus <leonshustov1995@gmail.com>
2022-02-17 19:47:50 +0300

    Add some styles + button to login page

--- src/pages/p1-login/login-page.tsx
+++ src/pages/p1-login/login-page.tsx
@@ -1,10 +1,18 @@
 import React from "react"
+import {CustomInput} from "../../common/c2-components/c3-input/CustomInput";
+import {CustomButton} from "../../common/c2-components/c4-button/CustomButton";
 
 
 export const LoginPage = () => {
     return(
         <div>
-            Login
+            <form>
+                <CustomInput/>
+                <CustomInput/>
+                <CustomButton>
+                    Sing in
+                </CustomButton>
+            </form>
         </div>
     )
 }
