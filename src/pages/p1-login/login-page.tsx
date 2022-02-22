2315547a47449c3b143b6333b7a51d3e1f71ec43
LeonShus <leonshustov1995@gmail.com>
2022-02-22 12:44:38 +0300

    Some styling login page

--- src/pages/p1-login/login-page.tsx
+++ src/pages/p1-login/login-page.tsx
@@ -1,14 +1,27 @@
 import React from "react"
+import styles from "./login-page.module.scss"
 import {CustomInput} from "../../common/c2-components/c3-input/CustomInput";
 import {CustomButton} from "../../common/c2-components/c4-button/CustomButton";
+import {CustomCheckbox} from "../../common/c2-components/c5-checkbox/CustomCheckbox";
 
 
 export const LoginPage = () => {
     return(
-        <div>
+        <div className={styles.container}>
+
             <form>
-                <CustomInput/>
-                <CustomInput/>
+                <CustomInput
+                    labelText={"Login"}
+                    placeholder={"Email"}
+                />
+                <CustomInput
+                    labelText={"password"}
+                    type={"password"}
+                    placeholder={"Password"}
+                />
+                <CustomCheckbox>
+                    Remember Me
+                </CustomCheckbox>
                 <CustomButton>
                     Sing in
                 </CustomButton>
